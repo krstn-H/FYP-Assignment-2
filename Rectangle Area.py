a9e933d9cb82d01e82dd1ff725553622cefc77d6
krstn-H <karsten.hans.polim@gmail.com>
2025-09-01 17:14:51 +0700

    Add files via upload

---
+++ Rectangle Area.py
@@ -0,0 +1,14 @@
+# Exercise 1 Rectangle Area calc
+name = "Karsten"
+lang = "Python"
+print(f"My name is : {name}")
+print(f"My favorite programming language is : {lang}")
+
+s_length = float(input("Rectangle length is?"))
+s_width = float(input("The Rectangle length is?"))
+s_area = s_length * s_width
+
+
+print(f"_ {s_length}")
+print(f"_ {s_width}")
+print(f"The Area is {s_area}cm^2")
